da53f3b4b28766edc3fb4436f6c9f953fb855566
a0323i1thaison <sontds170551@fpt.edu.vn>
2024-04-15 18:47:57 +0700

    nopbai

--- MySQL/ss4/cac_ham_thong_dung.sql
+++ MySQL/ss4/cac_ham_thong_dung.sql
@@ -1,1 +1,14 @@
-use c1023i1_quan_ly_sinh_vien ;
+use ql_sinh_vien ;
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+SELECT *
+FROM subject
+WHERE Credit = (SELECT MAX(Credit) FROM subject);
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+SELECT * FROM subject
+WHERE SubID IN (SELECT SubID FROM mark WHERE Mark = (SELECT MAX(Mark) FROM mark));
+-- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+SELECT Students.StudentID, Students.StudentName, AVG(mark.Mark) AS AverageMark
+FROM Students
+LEFT JOIN mark ON Students.StudentID = mark.StudentID
+GROUP BY Students.StudentID, Students.StudentName
+ORDER BY AverageMark DESC;
